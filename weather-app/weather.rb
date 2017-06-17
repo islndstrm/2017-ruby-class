4d746bc9cbf630268f60a7fd1bfec6399dd19343
islndstrm <lizetchells@gmail.com>
2017-06-16 21:54:54 -0400

    weather app changes

--- weather-app/weather.rb
+++ weather-app/weather.rb
@@ -1,13 +1,12 @@
-# print "Please enter your 5-digit zip code: "
-# location = gets.chomp
-source 'http://www.wunderground.com'
-
 require 'barometer'
 
 # use yahoo and wunderground
 Barometer.config = { 1 => [:wunderground] }
 
-barometer = Barometer.new('90210')
+print "Please enter your 5-digit zip code: "
+location = gets.chomp
+
+barometer = Barometer.new(location)
 weather = barometer.measure
 
-puts weather.temperture
+puts weather.temperature
